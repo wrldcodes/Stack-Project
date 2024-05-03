6b9db4851593de60142659bafaa4fdaf8dabf135
wrldcodes <jubrilelebute36@gmail.com>
2024-05-03 11:54:38 +0100

    deleted all assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 function NavBar() {
     return (
         <div>
-             < h1 className='flex'>Fighting Demons </h1>
-        <ul className='flex right space-x-8 justify-center lg:flex-col ' >
-            <li>Home</li>
-            <li>Ratings</li>
-            <li>About</li>
+            
         </ul>
         <div>
          
